a4e3da313465f37f697ed3af2b6d40d8d7889da2
cidborba <cidfernando@gmail.com>
2025-03-21 14:21:51 -0300

    Adicionada também funcionalidade básica ao wireframe. Readme atualizado

--- src/graphical_objects/wireframe.py
+++ src/graphical_objects/wireframe.py
@@ -1,3 +1,38 @@
 #todo
-class Wireframe:
-    pass
+from graphical_objects.abstract_graphical_object import AbstractGraphicalObject
+
+class Wireframe(AbstractGraphicalObject):
+    def __init__(self, name, coordinates: list[tuple[str, str]]):
+        super().__init__(name)
+        self.coordinates = coordinates
+        print(f"Wireframe {name} created at {coordinates}")
+
+    def get_object_center(self):
+        return self.coordinates
+    
+    def get_vertices(self):
+        return [self.coordinates]
+    
+    def get_type(self):
+        return "Wireframe"
+    
+    def modify(self, new_coords):
+        self.coordinates = new_coords
+        return self
+    
+    def draw(self, canvas):
+        for i in range(len(self.coordinates) - 1):
+            viewport_x0, viewport_y0 = canvas.world_to_viewport(*self.coordinates[i])
+            viewport_x1, viewport_y1 = canvas.world_to_viewport(*self.coordinates[i + 1])
+            canvas.create_line(
+                viewport_x0, viewport_y0,
+                viewport_x1, viewport_y1,
+                fill="black"
+            )
+        viewport_x0, viewport_y0 = canvas.world_to_viewport(*self.coordinates[-1])
+        viewport_x1, viewport_y1 = canvas.world_to_viewport(*self.coordinates[0])
+        canvas.create_line(
+            viewport_x0, viewport_y0,
+            viewport_x1, viewport_y1,
+            fill="black"
+        )
--- src/todo.md
+++ src/todo.md
@@ -1,6 +1,8 @@
 - O parsing de coordenadas em utils por enquanto apenas aceita coordenadas do tipo x, y para criar pontos, assim não dá para criar retas ou polígonos, consertar isso.
+#parsing aceita está aceitando mais pontos, mas ainda no formato <x0, y0, x1, y1, x2, y2>
 
 - Implementar line e wireframe; line sendo composto por dois pontos e wireframe por N lines
+#wireframe colocado como uma lista de pontos
 
 - Adicionar demais botões à UI como o de mover um objeto e também uma lista que demonstra quais os objetos atuais criados
 
