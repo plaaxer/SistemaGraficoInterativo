6e7abea182176244fb9e4cc4c3a62dbcd6c8fe69
Cid <cidfernando@gmail.com>
2025-05-06 19:25:27 -0300

    Rotação window 3D, desenho de objetos 3D

--- src/viewport.py
+++ src/viewport.py
@@ -12,6 +12,8 @@ class Viewport(Canvas):
         # coordenadas da janela
         self.window_bounds = c.WINDOW_BOUNDS
         self.vup = c.VIEW_UP_VECTOR
+        self.vrp = c.VIEW_REFERENCE_POINT
+        self.vpn = c.VIEW_PLANE_NORMAL
         self.margin = 0.05  # margem da janela de clipping
 
         self.window_angle = 0
@@ -38,7 +40,7 @@ class Viewport(Canvas):
 
         return viewport_x, viewport_y
     
-    def translate_window(self, dwx, dwy):
+    def translate_window(self, dwx, dwy, dwz):
 
             print("Requested translation:", dwx, dwy)
 
@@ -72,20 +74,43 @@ class Viewport(Canvas):
             self.update()
 
 
-    def rotate_window(self, angle: int):
+    def rotate_window(self, angle_x=0, angle_y=0, angle_z=0):
+        """
+        Rotaciona a window em torno dos eixos X, Y e Z.
+        """
+        print("Requested rotation:", angle_x, angle_y, angle_z)
 
-        # apenas para referência no futuro
-        self.window_angle = (self.window_angle + angle) % 360
+        def rotation_matrix(axis, angle):
+            angle = np.radians(angle)
+            c, s = np.cos(angle), np.sin(angle)
+            if axis == 'x':
+                return np.array([[1, 0, 0], [0, c, -s], [0, s, c]])
+            elif axis == 'y':
+                return np.array([[c, 0, s], [0, 1, 0], [-s, 0, c]])
+            elif axis == 'z':
+                return np.array([[c, -s, 0], [s, c, 0], [0, 0, 1]])
 
-        # rotaciona o vup
-        rad = np.radians(angle)
-        cos_a = np.cos(rad)
-        sin_a = np.sin(rad)
-        vx, vy = self.vup
-        rotated_vup = (vx * cos_a - vy * sin_a, vx * sin_a + vy * cos_a)
-        norm = np.linalg.norm(rotated_vup)
-        if norm != 0:
-            self.vup = (rotated_vup[0] / norm, rotated_vup[1] / norm)
+        # Rotação em torno do eixo X
+        if angle_x != 0:
+            rot_x = rotation_matrix('x', angle_x)
+            self.vpn = np.dot(rot_x, self.vpn)
+            self.vup = np.dot(rot_x, self.vup)
+
+        # Rotação em torno do eixo Y
+        if angle_y != 0:
+            rot_y = rotation_matrix('y', angle_y)
+            self.vpn = np.dot(rot_y, self.vpn)
+            self.vup = np.dot(rot_y, self.vup)
+
+        # Rotação em torno do eixo Z
+        if angle_z != 0:
+            rot_z = rotation_matrix('z', angle_z)
+            self.vpn = np.dot(rot_z, self.vpn)
+            self.vup = np.dot(rot_z, self.vup)
+
+        # Normaliza os vetores
+        self.vpn = self.vpn / np.linalg.norm(self.vpn)
+        self.vup = self.vup / np.linalg.norm(self.vup)  
 
         self.update()
 
@@ -139,92 +164,101 @@ class Viewport(Canvas):
     def update_specific_scn(self, obj):
             #print("---UPDATING SCN---")
 
-            # localização da window
-            x_min, y_min = self.window_bounds[0]
-            x_max, y_max = self.window_bounds[1]
-
-            # computa o centro da window
-            cx = (x_min + x_max) / 2
-            cy = (y_min + y_max) / 2
-
-            # tamanho da window
-            window_width = x_max - x_min
-            window_height = y_max - y_min
-
+        try:
             vertices = obj.get_vertices()
-
-            print("vertices in specific scn size: ", len(vertices))
-                
-            # faz a translação do mundo (nesse caso, 1 objeto) para o centro da window
-            translated = [(x - cx, y - cy) for x, y in vertices]
-
-            print("translated size: ", len(translated))
-
-            # o ângulo de rotação é o ângulo entre a VUP e o eixo Y do mundo
-            vx, vy = self.vup
-            angle = -np.arctan2(vx, vy)
-
-            cos_a, sin_a = np.cos(angle), np.sin(angle)
-
-            # rotaciona o mundo por -θ para alinhar o VUP com o eixo Y
-            rotated = [
-                (x * cos_a - y * sin_a, x * sin_a + y * cos_a)
-                for x, y in translated
+        except AttributeError:
+            # 1. Translada VRP para a origem
+            vrp = self.vrp
+            translated_vertices = [
+                (x - vrp[0], y - vrp[1], z - vrp[2]) for x, y, z in vertices
             ]
 
-            print("rotated size: ", len(rotated))
+            # 2. Determina VPN e seus ângulos com X e Y
+            vpn = self.vpn / np.linalg.norm(self.vpn)  # Normaliza VPN
+            theta_x = np.arctan2(vpn[1], vpn[2])  # Ângulo com o eixo X
+            theta_y = np.arctan2(vpn[0], vpn[2])  # Ângulo com o eixo Y
 
-            # normaliza de volta para scn
-            scn = [
-                ((x + window_width / 2) / window_width,
-                (y + window_height / 2) / window_height)
-                for x, y in rotated
+            # 3. Rotaciona o mundo em torno de X e Y para alinhar VPN com o eixo Z
+            # Rotação em torno de X
+            cos_theta_x, sin_theta_x = np.cos(-theta_x), np.sin(-theta_x)
+            rotation_x = np.array([
+                [1, 0, 0],
+                [0, cos_theta_x, -sin_theta_x],
+                [0, sin_theta_x, cos_theta_x]
+            ])
+            rotated_vertices = [np.dot(rotation_x, v) for v in translated_vertices]
+
+            # Rotação em torno de Y
+            cos_theta_y, sin_theta_y = np.cos(-theta_y), np.sin(-theta_y)
+            rotation_y = np.array([
+                [cos_theta_y, 0, sin_theta_y],
+                [0, 1, 0],
+                [-sin_theta_y, 0, cos_theta_y]
+            ])
+            rotated_vertices = [np.dot(rotation_y, v) for v in rotated_vertices]
+
+            # 4. Ignora as coordenadas Z dos objetos
+            projected_vertices = [(x, y) for x, y, z in rotated_vertices]
+            # 5. Normaliza as coordenadas restantes
+            x_min, y_min = self.window_bounds[0][:2]
+            x_max, y_max = self.window_bounds[1][:2]
+            cx, cy = (x_min + x_max) / 2, (y_min + y_max) / 2
+            window_width, window_height = x_max - x_min, y_max - y_min
+
+            normalized_vertices = [
+                ((x - cx) / window_width, (y - cy) / window_height)
+                for x, y in projected_vertices
             ]
 
-            print("scn in update size: ", len(scn))
-
-            obj.set_scn_vertices(scn)
-    
-    def desnormalizar_vertices(self, scn_vertices, window_bounds, vup):
-
+            # 7. Transforma para coordenadas da viewport
+            for x, y in normalized_vertices:
+                self.window_to_viewport(x, y)
+            return
+        
         # localização da window
-        x_min, y_min = window_bounds[0]
-        x_max, y_max = window_bounds[1]
+        x_min, y_min = self.window_bounds[0]
+        x_max, y_max = self.window_bounds[1]
 
         # computa o centro da window
         cx = (x_min + x_max) / 2
         cy = (y_min + y_max) / 2
 
-        # ângulo entre a VUP e o eixo Y do mundo (mesmo cálculo que antes)
-        vx, vy = vup
-        angle = -np.arctan2(vx, vy)
-
-        cos_a, sin_a = np.cos(-angle), np.sin(-angle)  # desfazendo a rotação
-
         # tamanho da window
         window_width = x_max - x_min
         window_height = y_max - y_min
 
-        # desfaz a normalização
+        print("vertices in specific scn size: ", len(vertices))
+            
+        # faz a translação do mundo (nesse caso, 1 objeto) para o centro da window
+        translated = [(x - cx, y - cy) for x, y in vertices]
+
+        print("translated size: ", len(translated))
+
+        # o ângulo de rotação é o ângulo entre a VUP e o eixo Y do mundo
+        vx = self.vup[0]
+        vy = self.vup[1]
+        angle = -np.arctan2(vx, vy)
+
+        cos_a, sin_a = np.cos(angle), np.sin(angle)
+
+        # rotaciona o mundo por -θ para alinhar o VUP com o eixo Y
         rotated = [
-            (x * window_width - window_width / 2,
-            y * window_height - window_height / 2)
-            for x, y in scn_vertices
-        ]
-
-        # desfaz a rotação (rotaciona por +θ)
-        translated = [
             (x * cos_a - y * sin_a, x * sin_a + y * cos_a)
-            for x, y in rotated
-        ]
-
-        # desfaz a translação (move o centro de volta)
-        original = [
-            (x + cx, y + cy)
             for x, y in translated
         ]
 
-        return original
+        print("rotated size: ", len(rotated))
+
+        # normaliza de volta para scn
+        scn = [
+            ((x + window_width / 2) / window_width,
+            (y + window_height / 2) / window_height)
+            for x, y in rotated
+        ]
+
+        print("scn in update size: ", len(scn))
+
+        obj.set_scn_vertices(scn)
 
     
     def update_all_scn(self):
